505941cb2726d61d5ae086209734e0af5f48176a
DevvIlya <yi1lei6a3@gmail.com>
2024-06-24 23:58:24 +0300

    update frontend
    добавлена анимация главной страницы
    исправлены мелкие ошибки

--- cloudhaven/frontend/src/pages/MainPage.tsx
+++ cloudhaven/frontend/src/pages/MainPage.tsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import { ROUTE_LOGIN, ROUTE_REGISTER } from "../utils/consts";
+import '../css/MainPage.css'; // Импортируем CSS файл
 
 const MainPage = (): JSX.Element => (
   <>
-    Основная страница MainPage
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">Welcome to Cloud Storage</h1>
-      <div>
-        <Link to={ROUTE_LOGIN} className="text-blue-500">Register</Link> | 
-        <Link to={ROUTE_REGISTER} className="text-blue-500"> Login</Link>
+    <div className="main-page">
+      <div className="container mx-auto p-4">
+        <h1 className="cloud-text">WELCOME TO CLOUD HAVEN</h1>
+        <div>
+          <Link to={ROUTE_LOGIN} className="text-blue-500">Login</Link> | 
+          <Link to={ROUTE_REGISTER} className="text-blue-500"> Register</Link>
+        </div>
       </div>
     </div>
   </>
